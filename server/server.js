60385ed0be331d77ee04531877a915fb770f1b11
Atri Omar <atri.omar.2003@gmail.com>
2024-10-04 04:16:19 +0100

    fix port message

--- server/server.js
+++ server/server.js
@@ -30,4 +30,4 @@ attachEvents(io);
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log("Server is running on port 5000"));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
